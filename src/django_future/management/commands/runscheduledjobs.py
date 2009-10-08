59f5dcd6f4fe0500a011165ddb7f6ca0b3efa36c
Gintautas Miliauskas <gintautas@miliauskas.lt>
2009-10-09 01:14:39 +0300

    Added -d parameter to runscheduledjobs to automatically delete completed jobs.

--- src/django_future/management/commands/runscheduledjobs.py
+++ src/django_future/management/commands/runscheduledjobs.py
@@ -1,13 +1,20 @@
 """Run scheduled jobs."""
 
 import datetime
-from django.core.management.base import BaseCommand
+from optparse import make_option
+from django.core.management.base import NoArgsCommand
 
 from django_future import run_jobs
 
 
-class Command(BaseCommand):
+class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
+        make_option('--delete-completed', '-d', action='store_true',
+                    dest='delete_completed',
+                    help='Do not keep entries for completed jobs in the database.'),
+    )
     help = "Executes any outstanding scheduled jobs."
 
-    def handle(self, *args, **options):
-        run_jobs()
+    def handle(self, **options):
+        delete_completed = bool(options.get('delete_completed', False))
+        run_jobs(delete_completed=delete_completed)

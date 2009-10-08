63d1970bd3d97caf676b3a2d5777afb4509b54e6
Gintautas Miliauskas <gintautas@miliauskas.lt>
2009-10-09 01:38:29 +0300

    Bumped version to 0.1.4.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ latest_changes = '\n\n\n'.join(changes[:3])
 
 
 setup(name='django-future',
-    version='0.1.3',
+    version='0.1.4',
     description='Scheduled jobs in Django',
     long_description=open('README').read() + '\n\n' + latest_changes,
     author='Shrubbery Software',

3dc89b121c165ad3f5eb550bda59dc6ef67a57ef
Adomas Paltanavičius <adomas@shrubberysoft.com>
2009-10-09 15:37:08 +0300

    Nicer listing of scheduled jobs in the admin app.

--- src/django_future/admin.py
+++ src/django_future/admin.py
@@ -3,9 +3,27 @@ from django_future.models import ScheduledJob
 
 
 class ScheduledJobAdmin(admin.ModelAdmin):
+    """Admin customization for ScheduledJob model."""
 
-    list_display = ('time_slot_start', 'status', 'callable_name', 'args', 'kwargs')
-    list_filter = ('status', 'callable_name')
+    status_colors = {
+        'default': '#000',
+        'running': '#356AA0',
+        'failed': '#B02B2C',
+        'complete': '#006E2E',
+        'expired': '#888'
+    }
+
+    def colorful_status(self, obj):
+        color = self.status_colors['default']
+        if obj.status in self.status_colors:
+            color = self.status_colors[obj.status]
+        return '<strong style="color: %s">%s</strong>' % (color, obj.get_status_display())
+    colorful_status.short_description = 'Status'
+    colorful_status.allow_tags = True
+
+    list_display = ('time_slot_start', 'colorful_status', 'callable_name', 'args', 'kwargs')
+    list_filter = ('status',)
+    date_hierarchy = 'time_slot_start'
 
 # TODO: show (read-only) reprs of args and kwargs in job editor screen.
 

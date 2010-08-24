ef96f32efd0db78c6b4823c906c6f34189db73bc
Jannis Leidel <jannis@leidel.info>
2010-08-24 14:35:30 +0200

    Marked a bunch of strings for translation.

--- src/django_future/models.py
+++ src/django_future/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext_lazy as _
+
 from picklefield import PickledObjectField
 
 from django_future.utils import parse_timedelta
@@ -18,30 +20,36 @@ END_OF_TIME = datetime.datetime(2047, 9, 14)
 class ScheduledJob(models.Model):
 
     STATUSES = (
-        ('scheduled', 'Scheduled'),
-        ('running', 'Running'),
-        ('failed', 'Failed'),
-        ('complete', 'Complete'),
-        ('expired', 'Expired'),
+        ('scheduled', _('Scheduled')),
+        ('running', _('Running')),
+        ('failed', _('Failed')),
+        ('complete', _('Complete')),
+        ('expired', _('Expired')),
     )
 
-    time_slot_start = models.DateTimeField()
-    time_slot_end = models.DateTimeField()
-    execution_start = models.DateTimeField(blank=True, null=True)
-    status = models.CharField(choices=STATUSES, max_length=32,
+    time_slot_start = models.DateTimeField(_('time slot start'))
+    time_slot_end = models.DateTimeField(_('time slot end'))
+    execution_start = models.DateTimeField(_('execution start'), blank=True, null=True)
+    status = models.CharField(_('status'), choices=STATUSES, max_length=32,
                               default='scheduled')
 
-    content_type = models.ForeignKey(ContentType, blank=True, null=True)
-    object_id = models.PositiveIntegerField(blank=True, null=True)
+    content_type = models.ForeignKey(ContentType, blank=True, null=True, verbose_name=_('content type'))
+    object_id = models.PositiveIntegerField(_('object ID'), blank=True, null=True)
     content_object = generic.GenericForeignKey()
 
-    callable_name = models.CharField(max_length=255)
-    args = PickledObjectField()
-    kwargs = PickledObjectField()
-    error = models.TextField(blank=True, null=True)
-    return_value = models.TextField(blank=True, null=True)
+    callable_name = models.CharField(_('callable name'), max_length=255,
+        help_text=_('The callable to be executed may be specified in two ways: '
+        'Set the callable name to an identifier (mypackage.myapp.some_function). '
+        'Or specify an instance of a model as the content object and set '
+        'the callable name to a method name (do_job).'))
+    args = PickledObjectField(_('args'))
+    kwargs = PickledObjectField(_('kwargs'))
+    error = models.TextField(_('error'), blank=True, null=True)
+    return_value = models.TextField(_('return value'), blank=True, null=True)
 
     class Meta:
+        verbose_name = _('scheduled job')
+        verbose_name_plural = _('scheduled jobs')
         get_latest_by = 'time_slot_start'
         ordering = ['time_slot_start']
 
